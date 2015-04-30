63d232bdf73507eb6813bab0f015d5fbbcb458e7
dfcacc <dfcacc@gmail.com>
2015-04-29 17:07:17 -0700

    third commit, made in eclipse trying to push up directly

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,8 +2,9 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		//Trying to push from eclipse...
 		// TODO Auto-generated method stub
-		System.out.println("hello, I'm branch two and this was typed in elcipse. ");
+		System.out.println("hello, I'm branch two and this was typed in elcipse.  ");
 	}
 
 }
